f4e102b9dd5e3a498831222f1c7b38026942243f
Adil Del Rey <adilbeg1@gmail.com>
2023-09-02 13:23:33 +0300

    google and apple website linked to sign in with buttons using JavaScript

---
+++ login.js
@@ -0,0 +1,12 @@
+const googleBtn = document.getElementById('googleBtn');
+const appleBtn = document.getElementById('appleBtn');
+
+googleBtn.addEventListener('click', function() {
+  //This redirects the user to the google homepage
+  window.location.href = 'https://www.google.com';
+});
+
+appleBtn.addEventListener('click', function() {
+  //This redirects the user to the google homepage
+  window.location.href = 'https://www.apple.com';
+});
